4b50a6f385b2a1e0d056936e62d77330e0abef3d
manvir bhangu <manvirbhangu222@gmail.com>
2026-06-10 11:28:53 +0530

    python learning phase

--- compare.py
+++
@@ -1,8 +0,0 @@
-age=int(input("what's value of age "))
-y=18
-if age!=y:
-    print("you can vote ")
-
-else:
-            print("u might not vote")
-            
--- python day 1.py
+++
@@ -1,7 +0,0 @@
-#ask user for their age 
-name =input("What is your name ").strip().title()
-age = input("what is your age ").strip()
-first,last=name.split(" ")
-print (f"your name is {last}" )
-print("and age is :"+age)
-
